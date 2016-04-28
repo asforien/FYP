5445996be18fcc4a5c8282b2ef6adf6a2d42d6da
Jeremy Yap <jeremy.yapjl@gmail.com>
2016-04-28 18:23:15 +0800

    Add script for aggregating data by language group

---
+++ analysis/distribution.py
@@ -0,0 +1,84 @@
+import csv
+
+answers = {}
+subjectLanguages = {}
+subjectInterfaces = {}
+
+distributions = {}
+subjectResults = {}
+numTranscriptions = {}
+
+with open('answers', 'r',) as answerFile:
+	reader = csv.reader(answerFile, delimiter=',')
+
+	for row in reader:
+		answers[row[0]] = row[1]
+
+with open('subjects', 'r',) as subjectFile:
+	reader = csv.reader(subjectFile, delimiter=',')
+
+	for row in reader:
+		subjectLanguages[row[0]] = row[1]
+		subjectInterfaces[row[0]] = row[2]
+
+with open('transcriptions', 'r',) as transcriptionFile:
+	reader = csv.reader(transcriptionFile, delimiter=',')
+
+	for row in reader:
+		subject = row[5]
+		language = subjectLanguages[subject]
+		interface = subjectInterfaces[subject]
+
+		q_no = row[4]
+		answer = answers[q_no]
+		transcription = row[1]
+
+		# Group results
+		group = language + interface
+
+		if group not in distributions:
+			distributions[group] = [[0 for x in range(6)] for y in range(6)]
+			numTranscriptions[group] = 0
+
+		numTranscriptions[group] += 1
+
+		for t, a in zip(transcription, answer):
+			distributions[group][int(a) - 1][int(t) - 1] += 1
+
+		# Individual results
+		if subject not in subjectResults:
+			subjectResults[subject] = [0 for x in range(12)]
+
+		for t, a in zip(transcription, answer):
+			subjectResults[subject][int(a) + 5] += 1
+			if t == a:
+				subjectResults[subject][int(a) - 1] += 1
+
+for group in distributions:
+	correct = 0
+	total = 0
+	for row in distributions[group]:
+		total += sum(row)
+	for i in range(6):
+		correct += distributions[group][i][i]
+	print(group)
+	print(correct/total)
+	print(numTranscriptions[group] / 3)
+	print()
+
+with open('distribution.csv', 'w') as outputFile:
+	writer = csv.writer(outputFile, delimiter=',')
+
+	for group in distributions:
+		writer.writerow([group, numTranscriptions[group] / 3])
+		for row in distributions[group]:
+			writer.writerow(row)
+		writer.writerow("")
+
+with open('individual.csv', 'w') as outputFile:
+	writer = csv.writer(outputFile, delimiter=',')
+
+	for subject in subjectResults:
+		results = subjectResults[subject]
+		scores = [results[x] / results [x+6] for x in range(6)] + [sum(results[0:6]) / sum(results[6:12])]
+		writer.writerow([subject, subjectLanguages[subject]] + scores)
--- automatic-tone-recognition/extract_features.py
+++ automatic-tone-recognition/extract_features.py
@@ -1,38 +1,79 @@
 import os
 import csv
+import math
 
 pitch = {}
 intensity = {}
 syllables = []
-features = []
+feature_vectors = []
 
 for fn in os.listdir('praat'):
 	f = open('praat/' + fn, 'r')
 	reader = csv.reader(f, delimiter=' ')
 	pitch[fn] = [0]
 	intensity[fn] = [0]
+	next(reader)
 	for row in reader:
-		pitch[fn].append(row[1])
-		intensity[fn].append(row[2])
+		if row[1] == '--undefined--':
+			row[1] = 0
+		if row[2] == '--undefined--':
+			row[2] = 0
+		pitch[fn].append(float(row[1]))
+		intensity[fn].append(float(row[2]))
+
+# pitch normalization
+for key in pitch:
+	old_pitch = pitch[key][1:]
+	f_min = min([x for x in old_pitch if x != 0])
+	f_max = max(old_pitch)
+	new_pitch = [0]
+	for f0 in old_pitch:
+		if f0 == 0:
+			new_pitch.append(0)
+		else:
+			new_pitch.append(math.log(f0 / f_min, 10) / math.log(f_max / f_min, 10) * 4 + 1)
+	pitch[key] = new_pitch
 
 with open('syllable_alignments.csv') as inputFile:
 	reader = csv.reader(inputFile, delimiter=' ')
 	for row in reader:
-		syllables.append([row[0], row[1], row[2], row[5]])
+		syllables.append([row[0], row[1], row[2], row[5]]) # filename, start, duration, tone
 
 for s in syllables:
 	start = int(float(s[1]) * 100)
 	duration = int(float(s[2]) * 100)
+	unvoiced = 0
 
-	for i in range(start, start + duration + 1):
-		print(pitch[s[0]][i])
-	print()
+	feature_vector = []
 
-# for i, s in enumerate(syllables):
-# 	if (i > 0) and syllables[i][0] == syllables[i-1][0]: 
-# 		prev_syllable = syllables[i-1]
+	while pitch[s[0]][start] == 0 and start < len(pitch[s[0]]) - 1:
+		start += 1
+		unvoiced += 1
+		duration -= 1
 
-# 	curr_syllable = syllables[i]
+	for i in range(1, 9):
+		time = start + duration * i // 9
+		pitch_feature = pitch[s[0]][time]
+		feature_vector.append(pitch_feature)
 
-# 	if (i < len(syllables) - 1) and syllables[i][0] == syllables[i+1][0]: 
-# 		next_syllable = syllables[i+1]
+	for i in range(0, 3):
+		start = start + duration * i // 3
+		end = start + duration * (i+1) // 3
+		intensities = intensity[s[0]][start:end]
+		intensities = [x for x in intensities if x != -1]
+		if len(intensities) == 0:
+			intensities = [0]
+		average_intensity = sum(intensities) / len(intensities)
+		feature_vector.append(average_intensity)
+
+	feature_vector.append(unvoiced)
+	feature_vector.append(duration)
+
+	feature_vector.append(s[3])
+	feature_vectors.append(feature_vector)
+
+with open('features.csv', 'w') as outputFile:
+	writer = csv.writer(outputFile, delimiter=',')
+
+	for fv in feature_vectors:
+		writer.writerow(fv)
